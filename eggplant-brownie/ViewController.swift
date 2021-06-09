e2764f700684e89d529004f7799140ee4ad3220b
d-llirium <plimacarlos@gmail.com>
2021-06-09 11:23:03 -0300

    add variables to the action button Adicionar

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 //controla a interface do usuário
 class ViewController: UIViewController {
-    //MARK UP: - métodos
+//MARK UP: - métodos
     //mesmo nome do botão da interface
     //MARK UP: - @IBAction
-    @IBAction func adicionar(){
-        print("alimento: --")
+    @IBAction func adicionar(_ sender: Any) {
+        let nome: String = "churros"
+        let felicidade: String = "5"
+        print("comi \(nome) e fiquei com felicidade: \(felicidade)")
     }
+}
     /*
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,5 +25,5 @@ class ViewController: UIViewController {
     }
 */
 
-}
+
 
