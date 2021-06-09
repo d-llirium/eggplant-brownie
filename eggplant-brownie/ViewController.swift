a5ca422d4bc9bc44a34e1c9bc5bd6fdda5f1dcca
d-llirium <plimacarlos@gmail.com>
2021-06-09 11:59:14 -0300

    connect the variables to the Outlets

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 //controla a interface do usuário
 class ViewController: UIViewController {
-//MARK UP: - métodos
+//MARK: - declarar variávies
+    //@IBOutlet paa associar a variável a um campo
+    //só vou saber o valor dessa variáveis no TEMPO de execução, por isso eu coloco um ! para forçar um valor, CUIDADO com o FORCE UNWRAPPING ! , pq pode dar crash
+    @IBOutlet var nomeTextField: UITextField!
+    @IBOutlet weak var felicidadeTextField: UITextField!
+    //MARK: - métodos
     //mesmo nome do botão da interface
-    //MARK UP: - @IBAction
+    //MARK: - @IBAction
     @IBAction func adicionar(_ sender: Any) {
-        let nome: String = "churros"
-        let felicidade: String = "5"
+        //.text para ler o texto
+        let nome = nomeTextField.text
+        let felicidade = felicidadeTextField.text
         print("comi \(nome) e fiquei com felicidade: \(felicidade)")
     }
 }
