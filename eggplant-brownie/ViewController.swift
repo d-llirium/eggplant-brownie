01b7a7d8120c5d013a8c1baeda1f788085bbfd39
d-llirium <plimacarlos@gmail.com>
2021-06-09 10:57:25 -0300

    add functionality to the Button with IBAction func

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
-
+//controla a interface do usuário
 class ViewController: UIViewController {
-
+    //MARK UP: - métodos
+    //mesmo nome do botão da interface
+    //MARK UP: - @IBAction
+    @IBAction func adicionar(){
+        print("alimento: --")
+    }
+    /*
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+*/
 
 }
 
