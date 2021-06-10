c5882ee3dd9ee6fcc25916217c42fd46b7eb7a2f
d-llirium <plimacarlos@gmail.com>
2021-06-10 16:10:16 -0300

    add methods

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -1,1 +1,26 @@
 import UIKit
+//Este é o código principal do nosso projeto
+//MARK: - atributos
+let nome = "churros"
+let felicidade = 5
+let calorias = 79.5
+let vegetal = false
+//MARK: - métodos
+//método sem parâmetro
+func alimentoConsumido(){
+    print("1.O alimento consumido foi: \(nome)")
+}
+//instanciando/chamando
+alimentoConsumido()
+//método com parâmetro
+func alimentoConsumido(nome: String, caloria: Double){
+    print("2.O alimento consumido foi: \(nome), com calorias: \(caloria)")
+}
+//instanciando/chamando
+alimentoConsumido(nome: nome, caloria: calorias)
+//para ocultar o nome dos parâmetros nas hora de invocar
+func alimentoConsumido(_ nome: String, _ caloria: Double){
+    print("3.O alimento consumido foi: \(nome), com calorias: \(caloria)")
+}
+//instanciando/chamando
+alimentoConsumido(nome,calorias)
