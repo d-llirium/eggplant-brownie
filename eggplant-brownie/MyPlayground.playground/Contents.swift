21b3ba105b06c87948bd73919acad237e0c57412
d-llirium <plimacarlos@gmail.com>
2021-06-11 19:03:44 -0300

    add if let and guard let

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -1,12 +1,5 @@
 import UIKit
-//criar a primeira refeição
-let nome: String = "Macarrão"
-let felicidade: String = "5"
-//criar a segunda refeição
-let nome2: String = "Churros"
-let felicidade2: String = "4"
-//--------------------------
-//MARK: - classe (estrutura para agrupar as características)
+
 class Refeicao{
     //opcionais = pode ou não ter valor
     var nome: String?
@@ -14,10 +7,32 @@ class Refeicao{
 }
 //referncia/instancia/chama a classe
 let refeicao = Refeicao()
-refeicao.nome = "Macarrão"
-//como é opcional, ele vai sempre imprimir com o Opcional("....")
-print(refeicao.nome)
+//refeicao.nome = "Macarrão"
 
 //MARK: - forced unwrap CUIDADO
-//se não tiver uma variável associada e dar unwrap, o app vai dar CRASH
-print(refeicao.nome!)
+//evitando CRASH
+//se não for nula, ela imprime, caso seja nula não imprime e tb não dá CRASH
+if refeicao.nome != nil {
+    print(refeicao.nome!)
+}
+//MARK: - if let
+//cria uma constante nome se existir valor na variável opcional e evita usar o unwrap > mais seguro
+//mas a constante só pode ser usada dentro do scopo do if
+if let nome = refeicao.nome{
+    print(nome)
+}
+//MARK:- guard let
+//a constante existe até o método acabar
+func exibeNomeDaRefeicao(){
+    //no if let a constante só pode ser usada dentro do scopo do if
+    if let nome = refeicao.nome{
+        print(nome)
+    }
+    //no guard let a constante pode ser usada dentro do scopo do método
+    guard let nome = refeicao.nome else{
+        return
+    }
+    print(nome)
+}
+//invocando o método
+exibeNomeDaRefeicao()
