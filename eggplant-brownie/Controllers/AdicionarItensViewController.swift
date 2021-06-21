0cac1c060ff4eb9d4c393b91df0aa128098328d9
d-llirium <plimacarlos@gmail.com>
2021-06-21 15:21:50 -0300

    create xib file

---
+++ eggplant-brownie/Controllers/AdicionarItensViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdicionarItensViewController.swift
+//  eggplant-brownie
+//
+//  Created by Patricia de Lima Carlos on 21/06/21.
+//  Copyright © 2021 Patricia Carlos. All rights reserved.
+//
+
+import UIKit
+
+class AdicionarItensViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
