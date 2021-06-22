1020a3c6d727529e2c7c6a1601d0f2500c8d223f
d-llirium <plimacarlos@gmail.com>
2021-06-22 16:57:33 -0300

    show refecao.nome when longPress

--- eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -28,7 +28,10 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
     }
     @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer){
         if gesture.state == .began{
-            print("longPress Gesture")
+            let celula = gesture.view as! UITableViewCell
+            guard let indexPath = tableView.indexPath(for: celula) else { return }
+            let refeicao = refeicoes[indexPath.row]
+            print("refeicao: \(refeicao.nome)")
         }
     }
         
